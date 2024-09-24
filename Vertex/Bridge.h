147ab65bb4fb4a325eceb4b5e1c1db0328f8782a
alfiecg24 <alfie@alfiecg.uk>
2024-09-24 22:06:43 +0100

    Add project

---
+++ Vertex/Bridge.h
@@ -0,0 +1,14 @@
+//
+//  Bridge.h
+//  Vertex
+//
+//  Created by Alfie on 24/09/2024.
+//
+
+#ifndef Bridge_h
+#define Bridge_h
+
+#include "Exploit/exploit.h"
+
+
+#endif /* Bridge_h */
--- Vertex/ContentView.swift
+++ Vertex/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("Go") {
+                exploit_init()
+                exploit_deinit()
+            }
         }
         .padding()
     }
