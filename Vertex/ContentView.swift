4b98d0ae94829969307a3e78613c358015b7ec53
alfiecg24 <alfie@alfiecg.uk>
2024-09-24 21:51:56 +0100

    Initial Commit

---
+++ Vertex/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Vertex
+//
+//  Created by Alfie on 24/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Vertex/VertexApp.swift
@@ -0,0 +1,17 @@
+//
+//  VertexApp.swift
+//  Vertex
+//
+//  Created by Alfie on 24/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct VertexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
